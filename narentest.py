8a66419a79e871e9918b90350bfeb4018df46420
NarendraBabuTR <89962970+NarendraBabuTR@users.noreply.github.com>
2021-09-03 13:13:12 +0530

    Add files via upload

---
+++ narentest.py
@@ -0,0 +1,11 @@
+import requests, base64
+import json
+import pymsteams
+from tabulate import tabulate
+import sys
+import os
+
+
+on: [repository_dispatch]
+#Test for hello world
+Print ('Hello, World')
